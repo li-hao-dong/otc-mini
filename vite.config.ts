368556f9ab45b80f7988f8cf1dd80539ed238333
li-hao-dong <1990608687@qq.com>
2026-02-09 23:49:17 +0800

    feat: 版本更新提示

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,42 @@ import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [uni()],
-  server: {
-    proxy: {
-      "/apiAgent": {
-        target: "https://option.sunsmicro.com/api/v1", // 跨域目标
-        ws: true, // 允许WebSocket
-        changeOrigin: true, // 将主机标头的来源更改为目标 URL
-        rewrite: (path) => path.replace(/^\/apiAgent/, ""),
-      },
-      "/boardApiAgent": {
-        target: "https://option.sunsmicro.com/board-api/v1", // 跨域目标
-        ws: true, // 允许WebSocket
-        changeOrigin: true, // 将主机标头的来源更改为目标 URL
-        rewrite: (path) => path.replace(/^\/boardApiAgent/, ""),
-      },
+export default defineConfig(async() => {
+  const { webUpdateNotice } = await import('@plugin-web-update-notification/vite');
+  return {
+    plugins: [
+      uni(),
+      webUpdateNotice({
+        notificationProps: {
+          title: '版本提示',
+          description: '检测到系统版本升级，请刷新页面!',
+          buttonText: '刷新',
+          dismissButtonText: '忽略'
+        },
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: ['import', 'legacy-js-api'],
+        }
+      }
+    },
+    server: {
+      proxy: {
+        "/apiAgent": {
+          target: "https://option.sunsmicro.com/api/v1", // 跨域目标
+          ws: true, // 允许WebSocket
+          changeOrigin: true, // 将主机标头的来源更改为目标 URL
+          rewrite: (path) => path.replace(/^\/apiAgent/, ""),
+        },
+        "/boardApiAgent": {
+          target: "https://option.sunsmicro.com/board-api/v1", // 跨域目标
+          ws: true, // 允许WebSocket
+          changeOrigin: true, // 将主机标头的来源更改为目标 URL
+          rewrite: (path) => path.replace(/^\/boardApiAgent/, ""),
+        },
+      }
     }
   }
 });
